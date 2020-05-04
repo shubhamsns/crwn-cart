8ac0c6c6142a1e05ed8f1c2e1eba065322343934
Shubham Sananse <shubhamsananse9@gmail.com>
2020-05-05 00:50:23 +0530

    edited stripe credit card Description

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -48,7 +48,7 @@ const Checkout = () => {
       <div className="test-warning">
         *Please use the following test credit card for payments*
         <br />
-        4242 4242 4242 4242 -Exp: 05/20 -CVV: 123
+        4242 4242 4242 4242 -Exp: 'Future Date' -CVV: 123
       </div>
       <StripeCheckoutButton price={total} />
     </div>
